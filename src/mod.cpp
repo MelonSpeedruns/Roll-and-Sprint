8ddf7d7b31702e99c86e1f8257740c567921f811
MelonSpeedruns <melonspeedruns@outlook.com>
2026-09-13 16:17:07 -0400

    sprint and roll initial commit

--- src/mod.cpp
+++ src/mod.cpp
@@ -1,42 +1,93 @@
 #include "mods/hook.hpp"
 #include "mods/service.hpp"
 #include "mods/svc/hook.h"
-#include "mods/svc/log.h"
 
-// Game includes
-#include "d/d_item_data.h"
-#include "f_op/f_op_actor_mng.h"
+#include "d/actor/d_a_alink.h"
+#include "d/actor/d_a_horse.h"
+#include "d/actor/d_a_midna.h"
+#include "d/d_com_inf_game.h"
 
 DEFINE_MOD();
-
-IMPORT_SERVICE(LogService, svc_log);
 IMPORT_SERVICE(HookService, svc_hook);
 
-// Example game hook: turn heart drops into green rupees.
-DEFINE_HOOK(fopAcM_createItem, CreateItem);
+DEFINE_HOOK(&daAlink_c::procMoveInit, LinkProcMoveInit);
+DEFINE_HOOK(&daAlink_c::setDoubleAnime, LinkSetDoubleAnime);
+DEFINE_HOOK(&daAlink_c::checkNormalAction, LinkCheckCutAction);
 
-static HookAction on_create_item_pre(ModContext*, void* args, void*, void*) {
-    int& itemNo = mods::arg_ref<int>(args, 1);
-    if (itemNo == dItemNo_HEART_e) {
-        itemNo = dItemNo_GREEN_RUPEE_e;
+bool running = false;
+
+extern "C" {
+
+HookAction link_proc_move_init_pre(ModContext* ctx, void* args, void* retval, void*) {
+    daAlink_c* link = daAlink_getAlinkActorClass();
+    if (!running && link && link->mProcID == daAlink_c::daAlink_PROC::PROC_FRONT_ROLL) {
+        if (mDoCPd_c::getHoldA(0) != 0 && !link->checkEventRun())
+        {
+            if (link->mEquipItem != 0xFF) {
+                link->allUnequip(0);
+            }
+
+            link->setSwordVoiceSe(Z2SE_AL_V_THROW_IB);
+            running = true;
+        }
     }
     return HOOK_CONTINUE;
 }
 
-extern "C" {
-MOD_EXPORT ModResult mod_initialize(ModError*) {
-    // Installs a pre hook on fopAcM_createItem.
-    ModResult result = mods::hook_add_pre<CreateItem>(svc_hook, on_create_item_pre);
-    if (result != MOD_OK) {
-        svc_log->error(mod_ctx, "failed to install on_create_item_pre");
-        return result;
-    }
+HookAction link_set_double_anime_pre(ModContext* ctx, void* args, void*, void*) {
+    daAlink_c* link = daAlink_getAlinkActorClass();
+    if (link && running) {
+        daAlink_c::daAlink_ANM& linkAnm = mods::arg_ref<daAlink_c::daAlink_ANM>(args, 5);
 
-    svc_log->info(mod_ctx, "my_mod initialized");
+        if (linkAnm == link->ANM_RUN) {
+            f32& anmSpeed = mods::arg_ref<f32>(args, 3);
+            anmSpeed = 2.0f;
+            linkAnm = link->ANM_RUN_B;
+            link->mNormalSpeed = 40.0f;
+        }
+    }
+    return HOOK_CONTINUE;
+}
+
+HookAction link_check_cut_action_pre(ModContext* ctx, void* args, void* retval, void*) {
+    daAlink_c* link = daAlink_getAlinkActorClass();
+    if (link && running && link->swordSwingTrigger()) {
+        if (!link->checkSwordEquipAnime()) {
+            if (link->checkWoodSwordEquip()) {
+                link->seStartSwordCut(Z2SE_AL_ITEM_TAKEOUT_FAST);
+            } else {
+                link->seStartSwordCut(Z2SE_AL_SWORD_PULLOUT);
+            }
+        }
+
+        link->mEquipItem = 0x103;
+        link->setItemModel();
+        link->procCutJumpInit(FALSE);
+        running = false;
+
+        if (retval != nullptr) {
+            *static_cast<int*>(retval) = 1;
+        }
+
+        return HOOK_SKIP_ORIGINAL;
+    }
+    return HOOK_CONTINUE;
+}
+
+MOD_EXPORT ModResult mod_initialize(ModError*) {
+    mods::hook::add_pre<LinkProcMoveInit>(link_proc_move_init_pre);
+    mods::hook::add_pre<LinkSetDoubleAnime>(link_set_double_anime_pre);
+    mods::hook::add_pre<LinkCheckCutAction>(link_check_cut_action_pre);
     return MOD_OK;
 }
 
 MOD_EXPORT ModResult mod_update(ModError*) {
+    daAlink_c* link = daAlink_getAlinkActorClass();
+    if (running && mDoCPd_c::getHoldA(0) == 0 || (link && link->checkEventRun()) ||
+        (link && link->mProcID != daAlink_c::daAlink_PROC::PROC_MOVE))
+    {
+        running = false;
+    }
     return MOD_OK;
 }
 
