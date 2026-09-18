e4ec77bfa93dc542c6f8555e541bc6fa8b1e874b
MelonSpeedruns <melonspeedruns@outlook.com>
2026-09-18 18:24:28 -0400

    Disable jump slashes if no sword is equipped. Option to re-enable it.

--- src/mod.cpp
+++ src/mod.cpp
@@ -13,8 +13,10 @@ DEFINE_HOOK(&daAlink_c::procMoveInit, LinkProcMoveInit);
 DEFINE_HOOK(&daAlink_c::setDoubleAnime, LinkSetDoubleAnime);
 DEFINE_HOOK(&daAlink_c::checkNormalAction, LinkCheckCutAction);
 
-UiElementHandle statusText = 0;
-ConfigVarHandle var = 0;
+UiElementHandle statusText1 = 0;
+UiElementHandle statusText2 = 0;
+ConfigVarHandle var1 = 0;
+ConfigVarHandle var2 = 0;
 
 bool running = false;
 
@@ -43,7 +45,7 @@ HookAction link_set_double_anime_pre(ModContext* ctx, void* args, void*, void*) 
 
         if (linkAnm == link->ANM_RUN) {
             int64_t speed = 100;
-            svc_config->get_int(mod_ctx, var, &speed);
+            svc_config->get_int(mod_ctx, var1, &speed);
             f32& anmSpeed = mods::arg_ref<f32>(args, 3);
             anmSpeed = 2.0f * (speed / 100.0f);
             linkAnm = link->ANM_RUN_B;
@@ -56,7 +58,15 @@ HookAction link_set_double_anime_pre(ModContext* ctx, void* args, void*, void*) 
 HookAction link_check_cut_action_pre(ModContext* ctx, void* args, void* retval, void*) {
     daAlink_c* link = daAlink_getAlinkActorClass();
     if (link && running && link->swordSwingTrigger()) {
-        if (!link->checkSwordEquipAnime()) {
+        u16 equipSword = dComIfGs_getSelectEquipSword();
+
+        bool allowNoSword = false;
+        svc_config->get_bool(mod_ctx, var2, &allowNoSword);
+        if (!allowNoSword && equipSword == 0xFF) {
+            return HOOK_CONTINUE;
+        }
+
+        if (!link->checkSwordEquipAnime() && equipSword != 0xFF) {
             if (link->checkWoodSwordEquip()) {
                 link->seStartSwordCut(Z2SE_AL_ITEM_TAKEOUT_FAST);
             } else {
@@ -81,31 +91,46 @@ HookAction link_check_cut_action_pre(ModContext* ctx, void* args, void* retval, 
 ModResult build(ModContext*, UiElementHandle panel, void*, ModError*) {
     svc_ui->pane_add_section(mod_ctx, panel, "Settings");
 
-    UiControlDesc control = UI_CONTROL_DESC_INIT;
-    control.kind = UI_CONTROL_NUMBER;
-    control.label = "Speed Multiplier (%)";
-    control.help_rml = "Shown in the help pane while focused.";
-    control.binding = UI_BINDING_CONFIG_VAR;
-    control.config_var = var;  // from svc_config->register_var
-    control.min = 80;
-    control.max = 200;
-    control.step = 5;
-    svc_ui->pane_add_control(mod_ctx, panel, &control, &statusText);
+    UiControlDesc control1 = UI_CONTROL_DESC_INIT;
+    control1.kind = UI_CONTROL_NUMBER;
+    control1.label = "Speed Multiplier (%)";
+    control1.help_rml = "The % of speed at which Link sprints at. Default is 100%.";
+    control1.binding = UI_BINDING_CONFIG_VAR;
+    control1.config_var = var1;  // from svc_config->register_var
+    control1.min = 80;
+    control1.max = 200;
+    control1.step = 5;
+    svc_ui->pane_add_control(mod_ctx, panel, &control1, &statusText1);
+
+    UiControlDesc control2 = UI_CONTROL_DESC_INIT;
+    control2.kind = UI_CONTROL_TOGGLE;
+    control2.label = "Allow Jump Slash Without Sword";
+    control2.help_rml = "Allow Jump Slashing even if you don't have a sword equipped.";
+    control2.binding = UI_BINDING_CONFIG_VAR;
+    control2.config_var = var2;  // from svc_config->register_var
+    svc_ui->pane_add_control(mod_ctx, panel, &control2, &statusText2);
 
     return MOD_OK;
 }
 
 ModResult update(ModContext*, void*, ModError*) {
-    svc_ui->elem_set_text(mod_ctx, statusText, "running");
+    svc_ui->elem_set_text(mod_ctx, statusText1, "running");
+    svc_ui->elem_set_text(mod_ctx, statusText2, "running");
     return MOD_OK;
 }
 
 MOD_EXPORT ModResult mod_initialize(ModError*) {
-    ConfigVarDesc desc = CONFIG_VAR_DESC_INIT;
-    desc.name = "speedMultiplier";  // 1-64 chars from [A-Za-z0-9_-]; "enabled" is reserved
-    desc.type = CONFIG_VAR_INT;
-    desc.default_int = 100;
-    svc_config->register_var(mod_ctx, &desc, &var);
+    ConfigVarDesc desc1 = CONFIG_VAR_DESC_INIT;
+    desc1.name = "speedMultiplier";
+    desc1.type = CONFIG_VAR_INT;
+    desc1.default_int = 100;
+    svc_config->register_var(mod_ctx, &desc1, &var1);
+
+    ConfigVarDesc desc2 = CONFIG_VAR_DESC_INIT;
+    desc2.name = "allowWithoutSword";
+    desc2.type = CONFIG_VAR_BOOL;
+    desc2.default_bool = false;
+    svc_config->register_var(mod_ctx, &desc2, &var2);
 
     mods::hook::add_pre<LinkProcMoveInit>(link_proc_move_init_pre);
     mods::hook::add_pre<LinkSetDoubleAnime>(link_set_double_anime_pre);
